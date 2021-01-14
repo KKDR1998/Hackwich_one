ebb51379a345b874b06901a323b7ec4059c6d03c
User <user@admins-macbook-pro-2.local>
2021-01-14 03:46:13 -1000

    Initial Commit

---
+++ Hackwich_one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackwich_one
+//
+//  Created by User on 1/14/21.
+//  Copyright © 2021 Kalahiki Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
